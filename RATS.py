fde2bcf8c0d7acc1b536d8b3d26d972dc6d5af89
nbrehm <nicolas@brehms.ch>
2025-09-04 09:33:17 +0200

    working on exe and macos release

--- RATS.py
+++ RATS.py
@@ -2,7 +2,6 @@ from sys import argv, exit
 from PyQt5.QtWidgets import QApplication
 from Library.ProjectViewer.MainWindow import WidgetMain
 
-
 if __name__ == '__main__':
     app = QApplication(argv)
     widget = WidgetMain('UIFiles/projectWindow.ui')
